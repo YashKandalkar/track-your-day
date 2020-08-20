0c2157621ea76322d97daa95553663af86afd645
YashKandalkar <yashsk.yk@gmail.com>
2020-08-20 13:56:45 +0530

    Updates

---
+++ precache-manifest.8a6a07749f05ca5d1050d521ab1e1fc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0ad84b2b62bb3e11ff6cb4503b4177",
+    "url": "/track-your-day/index.html"
+  },
+  {
+    "revision": "d0036651f5b85937d367",
+    "url": "/track-your-day/static/css/main.595a3bd5.chunk.css"
+  },
+  {
+    "revision": "1f0a736937d285fd7282",
+    "url": "/track-your-day/static/js/2.3f461030.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/track-your-day/static/js/2.3f461030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0036651f5b85937d367",
+    "url": "/track-your-day/static/js/main.4a703292.chunk.js"
+  },
+  {
+    "revision": "a7cc3cfa51cbe0a37516",
+    "url": "/track-your-day/static/js/runtime-main.ed9a6432.js"
+  }
+]);
